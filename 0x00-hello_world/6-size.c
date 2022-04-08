3ebc0bc89b9a22f5dc9ce13cd26e7f328324eb41
Emmirate <okaforemmanuel808@gmail.com>
2022-04-08 14:59:17 +0100

    My task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include (stdio.h)
+
+/**
+ * main -E[D Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+long int b;
+long long int c;
+char d;
+float f;
+
+printf("size of char: %lu  byte(s)\n", (unsigned long)sizeof(d));
+[A[C[C[C[C[C[C[C[C[S[B[A[D[D[D[D[D[D[D[D[D[Bprintf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
+}
