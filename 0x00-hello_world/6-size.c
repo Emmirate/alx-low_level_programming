93609c20a5fda5f2f1dc0076b5208b7c45ed32ff
Emmirate <okaforemmanuel808@gmail.com>
2022-04-13 00:54:19 +0100

    My first readme

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include (stdio.h)
 
 /**
- * main -E[D Entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -14,7 +14,7 @@ char d;
 float f;
 
 printf("size of char: %lu  byte(s)\n", (unsigned long)sizeof(d));
-[A[C[C[C[C[C[C[C[C[S[B[A[D[D[D[D[D[D[D[D[D[Bprintf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
