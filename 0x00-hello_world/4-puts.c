2075da3caae58d40fd21707a227aefe7aa23505f
Emmirate <okaforemmauel808@gmail.com>
2022-04-16 18:28:01 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always  (su
+  * main - main block
+  * Return: 0
+  */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle")
+	return (0);
+}
