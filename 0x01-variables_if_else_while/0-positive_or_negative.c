5a0a627b65dd6a72b609d97165d601058c6af250
okafor emmanuel <101372666+Emmirate@users.noreply.github.com>
2022-04-22 02:38:05 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,34 +1,25 @@
-#include <stdlid.h>
-##include <time.h>
-[A[[#[C[C[C[C[C[C[C[C[C[C[Cinclude <time.h>[B[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[B#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
+/* betty style doc for function main goes there */
 /**
  * main - Entry point
  *
- * Return: Always 0 (success/correct)
- [1;5x[3;1;1;120;120;1;0x*/
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-int n:
+	int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2:
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n == 0)
+		printf("%i is zero\n", n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
 
-if (n==0)
-{
-printf("%i is zero\n", n):
-}
-
-else if (n<0)
-printf("%i isnegativ[D[D[D[D[D[D[D n[B[C[C[ negative\n", n);
-}
-
-else
-{
-printf("%i is positive\n", n):
-}
-
-return (0);
-
+	return (0);
 }
